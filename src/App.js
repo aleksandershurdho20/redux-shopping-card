4a4129408cdc4e9d027a849bf415c2290b5dcdbb
aleksandershurdho20 <aleksandershurdho@gmail.com>
2020-11-25 23:22:18 +0100

    added body component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from "./Components/Header/Header"
+import Index from "./Components/OrderCard/Index"
+
 // import "./Components/Header/Body.scss"
 function App() {
   return (
     <div className="App">
       <Header />
+      <Index />
     </div>
   );
 }
---
+++ src/Components/OrderCard/Index.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+        margin: 15,
+        background: 'white',
+        padding: '30px'
+    },
+    paper: {
+        padding: theme.spacing(2),
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+    },
+    card: {
+        height: '100vh',
+        padding: '50px',
+    }
+}));
+export default function Index() {
+    const classes = useStyles();
+
+    return (
+        <div className={classes.root}>
+            <Grid container spacing={3}>
+                <Grid item xs={12}>
+                    <Typography
+                        variant="h4"
+                        className={classes.brandText}
+                        display="inline"
+                        color="primary"
+                    >
+                        Your Shopping                        </Typography>
+                    <Typography
+                        variant="h4"
+                        className={classes.brandText}
+                        display="inline"
+                        style={{ color: '#4e3535' }}
+                    >
+                        BAG
+                        </Typography>
+                </Grid>
+                <Grid item xs={6}>
+                    <Paper className={classes.paper}>xs=6</Paper>
+                </Grid>
+                <Grid item xs={6}>
+                    <Paper className={classes.paper}>xs=6</Paper>
+                </Grid>
+
+            </Grid>
+
+        </div>
+    )
+}
